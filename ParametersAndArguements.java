4ac8eafb03cd43b9f6c6a64b8cf0e89fb57476a2
MarkjosephNayve <nayvemarkjoseph_bsit@plmun.edu.ph>
2025-02-19 00:47:14 +0800

    Submitting OBJPROG Week 05 - Laboratory # 13

---
+++ ParametersAndArguements.java
@@ -0,0 +1,15 @@
+public class ParametersAndArguements
+{
+    public static int calculateArea (int width, int height) {
+        int area = width * height;
+        return area;
+    }
+	public static void main(String[] args) {
+		int roonWidth = 10;
+		int roomHeight = 15;
+		
+		int area = calculateArea(roonWidth, roomHeight);
+		
+		System.out.println ("The calculateArea is: " + area );
+	}
+}
